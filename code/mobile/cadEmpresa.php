385a4614e3246b50f72e0a387f2ded1fecc8af41
Lara Aguilar <72893552+LarAguilar@users.noreply.github.com>
2022-01-20 16:06:16 -0300

    Update cadEmpresa.php

--- code/mobile/cadEmpresa.php
+++ code/mobile/cadEmpresa.php
@@ -13,10 +13,13 @@ include_once "conexao.php";
 $response = array();
  
 // check for required fields
-if (isset($_POST['newLogin']) && isset($_POST['newPassword'])) {
+if (isset($_POST['newLogin']) && (isset($_POST['newPassword'])) && (isset($_POST['CpfCnpj'])) && (isset($_POST['nomEmpresa'])) && (isset($_POST['telefone']))) {
  
 	$newLogin = trim($_POST['newLogin']);
 	$newPassword = trim($_POST['newPassword']);
+	$nomEmpresa = ($_POST['nomEmpresa']);
+	$CpfCnpj = trim($_POST['CpfCnpj']);
+	$telefone = trim($_POST['telefone']);
 	
 	$usuario_existe = mysqli_query($conn, "SELECT email FROM empresa WHERE email='$newLogin'");
 	// check for empty result
@@ -27,7 +30,7 @@ if (isset($_POST['newLogin']) && isset($_POST['newPassword'])) {
 	else {
 		$senhaCript = password_hash($newPassword, PASSWORD_DEFAULT);
 		// mysql inserting a new row
-		$result = mysqli_query($conn, "INSERT INTO empresa(email, senha) VALUES('$newLogin', '$senhaCript')");
+		$result = mysqli_query($conn, "INSERT INTO empresa(nomEmpresa, CpfCnpj,email, telefone, senha) VALUES('$nomEmpresa', '$CpfCnpj', '$newLogin', '$telefone','$senhaCript')");
 	 
 		if ($result) {
 			$response["success"] = 1;
