50ca5b66312825111ce341059c7ba6e2af9a79e7
firedomito <72893552+laraguilar@users.noreply.github.com>
2022-01-21 15:10:56 -0300

    Create get_data.php

---
+++ code/mobile/get_data.php
@@ -0,0 +1,52 @@
+<?php
+ 
+// array for JSON response
+$response = array();
+
+// conecta ao BD
+
+$con = mysqli_connect('DATABASE_URL');
+
+if(mysqli_connect_error()):
+    echo "Falha na conexão: ". mysqli_connect_error();
+endif;
+$username = NULL;
+$password = NULL;
+
+$isAuth = false;
+
+// Método para mod_php (Apache)
+if(isset( $_SERVER['PHP_AUTH_USER'])) {
+    $username = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
+} // Método para demais servers
+elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
+    if(preg_match( '/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
+		list($username, $password) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+}
+
+// Se a autenticação não foi enviada
+if(!is_null($username)){
+    $query = mysqli_query($con, "SELECT senha FROM empresa WHERE email='$username'");
+
+	if(mysqli_num_rows($query) > 0){
+		$row = mysqli_fetch_array($query);
+		if($password == $row['password']){
+			$isAuth = true;
+		}
+	}
+}
+ 
+if($isAuth) {
+	$response["success"] = 1;
+	
+	// codigo sql da sua consulta
+	$response["email"] = "Dados da app";
+}
+else {
+	$response["success"] = 0;
+	$response["error"] = "falha de autenticação";
+}
+mysqli_close($con);
+echo json_encode($response);
+?>
