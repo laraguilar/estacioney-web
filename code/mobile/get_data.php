d79fde172ee7aeda6a7f2d38e91c998658a3312e
firedomito <72893552+laraguilar@users.noreply.github.com>
2022-01-21 17:41:38 -0300

    Update get_data.php

--- code/mobile/get_data.php
+++ code/mobile/get_data.php
@@ -50,7 +50,7 @@ if ($isAuth) {
 	$logradouro = $endereco['dscLogradouro'];
 
 
-	$response["data"] = '$nomEstac'.'$logradouro';
+	$response["data"] = '$nomEstac';
 
 } else {
 	$response["success"] = 0;
