1513fc7c74a02554b177e9d91fad323e12d56213
firedomito <72893552+laraguilar@users.noreply.github.com>
2022-01-21 17:38:00 -0300

    Update get_data.php

--- code/mobile/get_data.php
+++ code/mobile/get_data.php
@@ -2,12 +2,12 @@
 
 // conecta ao BD
 include_once "conexao.php";
- 
+
 // array for JSON response
 $response = array();
 
-if(mysqli_connect_error()):
-    echo "Falha na conexão: ". mysqli_connect_error();
+if (mysqli_connect_error()) :
+	echo "Falha na conexão: " . mysqli_connect_error();
 endif;
 $username = NULL;
 $password = NULL;
@@ -15,40 +15,46 @@ $password = NULL;
 $isAuth = false;
 
 // Método para mod_php (Apache)
-if(isset( $_SERVER['PHP_AUTH_USER'])) {
-    $username = $_SERVER['PHP_AUTH_USER'];
-    $password = $_SERVER['PHP_AUTH_PW'];
+if (isset($_SERVER['PHP_AUTH_USER'])) {
+	$username = $_SERVER['PHP_AUTH_USER'];
+	$password = $_SERVER['PHP_AUTH_PW'];
 } // Método para demais servers
-elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
-    if(preg_match( '/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
+elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+	if (preg_match('/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
 		list($username, $password) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
 }
 
 // Se a autenticação não foi enviada
-if(!is_null($username)){
-    $query = mysqli_query($conn, "SELECT senha FROM empresa WHERE email='$username'");
+if (!is_null($username)) {
+	$query = mysqli_query($conn, "SELECT senha FROM empresa WHERE email='$username'");
 
-	if(mysqli_num_rows($query) > 0){
+	if (mysqli_num_rows($query) > 0) {
 		$row = mysqli_fetch_array($query);
-		if(password_verify($password, $row['senha'])){
+		if (password_verify($password, $row['senha'])) {
 			$isAuth = true;
 		}
 	}
 }
- 
-if($isAuth) {
+
+if ($isAuth) {
 	$response["success"] = 1;
-	
+
 	// codigo sql da sua consulta
-	$testando = "SELECT nomEstac, idEnd FROM estacionamento WHERE idEmpresa = '$id';";
-    $resultado = mysqli_query($conn, $testando);
-    $testeFinal = mysqli_fetch_array($resultado);
-	$response["data"] = $testeFinal;
-}
-else {
+	$query = mysqli_query($conn, "SELECT idEstac, nomEstac, idEnd from estacionamento");
+	$estacs = mysqli_fetch_array($query);
+	$idEstac = $estacs['idEstac'];
+	$nomEstac = $estacs['nomEstac'];
+
+	$query2 = mysqli_query($conn, "SELECT * from endereco where idEstac = '$idEstac'");
+	$endereco = mysqli_fetch_array($query2);
+	$logradouro = $endereco['dscLogradouro'];
+
+
+	$response["data"] = $nomEstac.$logradouro;
+
+} else {
 	$response["success"] = 0;
 	$response["error"] = "falha de autenticação";
 }
 mysqli_close($conn);
 echo json_encode($response);
-?>
